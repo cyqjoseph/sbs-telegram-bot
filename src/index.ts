f7ef57a41600d5e20c32ecf4df3804a48f8f4a6e
BunAppleTeeth <cyqjoseph@gmail.com>
2022-01-19 15:09:33 +0800

    updated procfile

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,4 @@
 const bot = require("./bot");
-const cron = require("node-cron");
-
-// cron.schedule("*/3 * * * * *", function () {
-//   console.log("called");
-//   bot.launch();
-//   process.once("SIGINT", () => bot.stop("SIGINT"));
-//   process.once("SIGTERM", () => bot.stop("SIGTERM"));
-// });
 
 const init = function (): void {
   bot.launch();
